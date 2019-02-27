bff772c3295c48ca9e3dcd3e0f1cca2cb2cda5cb
arjunprakash05 <arjunprakashemail@gmail.com>
2019-02-27 15:24:06 -0800

    Integrated Version

---
+++ LAMMPS with Neural Network/build/CMakeFiles/mpi_stubs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mpi_stubs.dir/lammps/src/STUBS/mpi.c.o"
+  "libmpi_stubs.pdb"
+  "libmpi_stubs.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mpi_stubs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ LAMMPS with Neural Network/build/etc/profile.d/lammps.sh
@@ -0,0 +1,2 @@
+# set environment for LAMMPS executables to find potential files
+export LAMMPS_POTENTIALS=${LAMMPS_POTENTIALS-/root/.local/share/lammps/potentials}
